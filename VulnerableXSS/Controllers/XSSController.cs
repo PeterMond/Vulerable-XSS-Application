df7c1202a0ea77224b5e39ca24233aeed546f360
Grant Shanklin <37347357+grants235@users.noreply.github.com>
2020-09-30 21:14:03 -0500

    Created Simple Reflected XSS page

---
+++ VulnerableXSS/Controllers/XSSController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using VulnerableXSS.Models;
+using System.Text.Encodings.Web;
+
+namespace VulnerableXSS.Controllers
+{
+    public class XSSController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult SimpleReflected()
+        {
+            XssBasicFields model = new XssBasicFields
+            {
+                NameVulnerable = " ",
+                NameFixed = " "
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult SimpleReflected(XssBasicFields model)
+        {
+            if(model.NameFixed == null)
+            {
+                model.NameFixed = " ";
+            }
+            if (model.NameVulnerable == null)
+            {
+                model.NameVulnerable = " ";
+            }
+            return View(model);
+        }
+    }
+}
---
+++ VulnerableXSS/Models/XssBasicFields.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VulnerableXSS.Models
+{
+    public class XssBasicFields
+    {
+        public string NameVulnerable { get; set; }
+        public string NameFixed { get; set; }
+    }
+}
